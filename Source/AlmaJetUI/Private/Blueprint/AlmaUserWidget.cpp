b9d89404cee5e9185a4d2e0c35a06232cd171a31
Andrey Protsenko <protsenko.ro.c@gmail.com>
2024-04-24 14:46:09 +0200

    Added AlmaUserWidget class

---
+++ Source/AlmaJetUI/Private/Blueprint/AlmaUserWidget.cpp
@@ -0,0 +1,5 @@
+// Copyright AlmaJet Game Studio, Inc. All Rights Reserved.
+
+#include "Blueprint/AlmaUserWidget.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(AlmaUserWidget)
---
+++ Source/AlmaJetUI/Public/Blueprint/AlmaUserWidget.h
@@ -0,0 +1,13 @@
+// Copyright AlmaJet Game Studio, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "Blueprint/UserWidget.h"
+#include "AlmaUserWidget.generated.h"
+
+UCLASS()
+class ALMAJETUI_API UAlmaUserWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
